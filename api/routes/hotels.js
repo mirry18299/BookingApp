9cdf78b8fa38b0ed295a812ea0da58146ebd09cd
mirry18299 <141658211+mirry18299@users.noreply.github.com>
2023-12-06 09:54:10 +0300

    booking api

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,29 +1,33 @@
-import  express  from "express";
+import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
-import { createHotel,
-         deleteHotel,
-         getHotel,
-         getHotels,
-         updateHotel,
-           } from "../controllers/hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
+const router = express.Router();
 
-const router = express.Router()
-
-
-
-    //CREATE
-    router.post("/", createHotel);
+//CREATE
+router.post("/", verifyAdmin, createHotel);
 
 //UPDATE
-router.put("/:id", updateHotel)
-
+router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id", deleteHotel)
-
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET
-router.get("/:id", getHotel)
 
+router.get("/find/:id", getHotel);
 //GET ALL
-router.get("/", getHotels)
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router;
